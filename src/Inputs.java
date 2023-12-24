e97c7e49e85121fe45d94648ceb2b5ed27e853b8
CyrilAshong <ashongcyril2704@gmail.com>
2023-12-24 22:56:37 +0000

    Java

---
+++ src/Inputs.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+          Scanner input = new Scanner(System.in);
+//        System.out.print("What's your roll number: ");
+//        int rollno = input.nextInt();
+//        System.out.println("Your roll number is: " + rollno);
+
+//        System.out.print("Enter your first Num: ");
+//        int firstNo = input.nextInt();
+//        System.out.print("Enter your second Num: ");
+//        int secondNo = input.nextInt();
+//
+//        int sum = firstNo + secondNo;
+//        System.out.println("The sum is: " + sum);
+
+        String name = input.nextLine();
+        System.out.println("Your name is: " + name);
+
+    }
+}
---
+++ src/Primitives.java
@@ -0,0 +1,9 @@
+public class Primitives {
+    public static void main(String[] args) {
+        int rollnum = 5678;
+        char letter = 'C';
+        float Decimal = 102.54f;
+        double LargeDecimalNo = 18293.8593842485;
+        long LargeIntNum = 5677683943434496L;
+    }
+}
