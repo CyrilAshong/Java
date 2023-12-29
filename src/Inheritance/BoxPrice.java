dc49b7b763c8a94645b1fe8fed963f8d6b357ae8
CyrilAshong <ashongcyril2704@gmail.com>
2023-12-29 03:03:10 +0000

    Java

---
+++ src/Inheritance/BoxPrice.java
@@ -0,0 +1,16 @@
+package Inheritance;
+
+public class BoxPrice extends BoxWeight {
+    double cost;
+
+    public BoxPrice (){
+        super();
+        this.cost = 500.40;
+    }
+
+    public BoxPrice(double l, double w, double h, double weight, double cost) {
+        super(l, w, h, weight);
+        this.cost = cost;
+    }
+
+}
--- src/Inheritance/Main.java
+++ src/Inheritance/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
         BoxWeight box1 = new BoxWeight(2.3, 5.6, 8.4, 3.1);
         BoxWeight box2 = new BoxWeight();
+        BoxPrice box3 = new BoxPrice();
 
-        System.out.println(box2.weight);
+        System.out.println(box3.cost);
     }
 }
